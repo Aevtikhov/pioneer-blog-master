ec1624ec4762ed443d783d8c1c83062977cb6edc
Anton_Evtikhov <Anton_Evtikhov@EPRUPETW7045>
2018-01-22 16:28:32 +0300

    First commit

---
+++ README.md
@@ -0,0 +1,88 @@
+# Pioneer Blog
+
+Active code based used for [Pioneer Code](https://pioneercode.com).
+
+Current stack...
+- ASP.NET Core 2.x
+- Entity Framework Core 2.x
+- Angular 5.x
+
+Current deployment...
+- SQL Database hosted at [WinHost](http://www.winhost.com/a/chadr)
+- Site hosted at [DigitalOcean](http://www.digitalocean.com/?refcode=d95b905a1279)
+  - Under Ubuntu and faced by NGINX
+
+## Admin 
+Full-featured admin portal.
+![alt text](assets/admin-screen.png)
+
+## Setup
+
+### Repository
+Clone the repository to your local environment.
+
+```bash
+git clone https://github.com/PioneerCode/pioneer-blog.git
+```
+
+### Configuration
+All configuration is derived from `appsettings.json`. That being said, it is recommended you create an `appsettings.development.json`  and `appsettings.production.json` file to override these settings.
+
+### Database
+
+- Update the connection string inside of `Pioneer.Blog\appsettings.json` & `Pioneer.DAL\appsettings.json`.
+- Open a command prompt at `Pioneer.Blog` and run the following
+
+```bash
+dotnet ef database update
+```
+
+At this point, a database and all corresponding tables should have been created in your database instance. 
+
+### Front-end
+
+Install [node](https://nodejs.org/en/) on your local environment and run the following.
+
+gulp
+```bash
+npm rm --global gulp
+npm install gulp-cli -g
+```
+
+typings
+```bash
+npm install typings -g
+```
+
+Navigate to the `Pioneer.Blog` directory from your command prompt and run....
+- `npm install`
+- `npm rebuild node-sass --force`
+- `gulp public`
+
+Navigate to to `Pioneer.Blog\Areas\admin-app` from your command prompt and run....
+- `npm install`
+- `npm start`
+
+### Run
+
+That is it! You now should be able to build and launch the project from your IDE of choosing. 
+
+### Registering A Super Account
+
+- Launch your application in debug mode.
+- Navigate to /account/register.
+	- Register a new account.
+- Open up your AspNetUserClaims and AspNetUsers table.
+- Add a new claim of the type **isSuperUser**
+	- Supply your newly registered UserId as the FK.
+
+![alt text](assets/claims.png)
+
+You can now use this account to perform all administrative tasks. 
+
+## What Is Next
+
+- If your curious what is coming down the pipe, I track features as [issues](https://github.com/PioneerCode/pioneer-blog/issues).
+- If there is interest, I am willing to entertain the idea of abstracting the vast majority of code away from the Pioneer Blog domain so that it can't be reused without to much refactoring.  
+
+## [CHANGELOG](CHANGELOG.md)
---
+++ src/Pioneer.Blog/Startup.cs
@@ -0,0 +1,191 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.PlatformAbstractions;
+using Pioneer.Blog.Entity;
+using Pioneer.Blog.Model;
+using Pioneer.Blog.Repository;
+using Pioneer.Blog.Service;
+using Pioneer.Pagination;
+
+namespace Pioneer.Blog
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<BlogDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddIdentity<UserEntity, IdentityRole>()
+                .AddEntityFrameworkStores<BlogDbContext>()
+                .AddDefaultTokenProviders();
+
+            ConfigureCookies(services);
+
+            services.AddAuthorization(cfg =>
+            {
+                cfg.AddPolicy("isSuperUser", p => p.RequireClaim("isSuperUser", "true"));
+            });
+
+            RegisterDependencies(services);
+
+            services.Configure<AppConfiguration>(Configuration.GetSection("AppConfiguration"));
+
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            ServiceMapperConfig.Config();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+                app.UseDatabaseErrorPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseStaticFiles();
+            app.UseAuthentication();
+
+            ConfigureMvc(app);
+        }
+
+        private static void ConfigureCookies(IServiceCollection services)
+        {
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.Events =
+                    new CookieAuthenticationEvents
+                    {
+                        OnRedirectToLogin = ctx =>
+                        {
+                            // If we were redirect to login from api..
+                            if (ctx.Request.Path.StartsWithSegments("/api") && ctx.Response.StatusCode == 200)
+                            {
+                                ctx.Response.StatusCode = 401;
+                                return Task.FromResult<object>(null);
+                            }
+
+                            ctx.Response.Redirect(ctx.RedirectUri);
+                            return Task.FromResult<object>(null);
+                        },
+                        OnRedirectToAccessDenied = ctx =>
+                        {
+                            // If access is denied from api...
+                            if (ctx.Request.Path.StartsWithSegments("/api") && ctx.Response.StatusCode == 200)
+                            {
+                                ctx.Response.StatusCode = 403;
+                                return Task.FromResult<object>(null);
+                            }
+
+                            ctx.Response.Redirect(ctx.RedirectUri);
+                            return Task.FromResult<object>(null);
+                        }
+                    };
+            });
+        }
+
+        private static void RegisterDependencies(IServiceCollection services)
+        {
+            // Third-party
+            services.AddTransient<IPaginatedMetaService, PaginatedMetaService>();
+
+            // Repositories
+            services.AddTransient<IContactRepository, ContactRepository>();
+            services.AddTransient<ICategoryRepository, CategoryRepository>();
+            services.AddTransient<ITagRepository, TagRepository>();
+            services.AddTransient<IPostRepository, PostRepository>();
+            services.AddTransient<IPostTagRepository, PostTagRepository>();
+
+            // Services
+            services.AddTransient<ICategoryService, CategoryService>();
+            services.AddTransient<IPostService, PostService>();
+            services.AddTransient<IPostTagService, PostTagService>();
+            services.AddTransient<ISearchService, SearchService>();
+            services.AddTransient<ICommunicationService, CommunicationService>();
+            services.AddTransient<ITagService, TagService>();
+            services.AddTransient<ISiteMapService, SiteMapService>();
+            services.AddTransient<ApplicationEnvironment>();
+        }
+
+        private static void ConfigureMvc(IApplicationBuilder app)
+        {
+            app.UseMvc(routes =>
+            {
+                // Areas support
+                routes.MapRoute(
+                    name: "areaRoute",
+                    template: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+                routes.MapRoute(
+                    name: "Post",
+                    template: "post/{id}",
+                    defaults: new { controller = "Post", action = "Index" });
+
+                routes.MapRoute(
+                    name: "BlogPost",
+                    template: "post",
+                    defaults: new { controller = "blog", action = "Index" });
+
+                routes.MapRoute(
+                    name: "BlogTag",
+                    template: "tag",
+                    defaults: new { controller = "blog", action = "Index" });
+
+                routes.MapRoute(
+                    name: "BlogCategory",
+                    template: "category",
+                    defaults: new { controller = "blog", action = "Index" });
+
+                routes.MapRoute(
+                    name: "Category",
+                    template: "category/{id}/{page?}",
+                    defaults: new { controller = "blog", action = "Category" });
+
+                routes.MapRoute(
+                    name: "Tag",
+                    template: "tag/{id}/{page?}",
+                    defaults: new { controller = "blog", action = "Tag" });
+
+                routes.MapRoute(
+                    name: "BlogFlat",
+                    template: "blog",
+                    defaults: new { controller = "blog", action = "Index" });
+
+                routes.MapRoute(
+                    name: "Blog",
+                    template: "blog/{page?}",
+                    defaults: new { controller = "blog", action = "Index" });
+
+                routes.MapRoute(
+                    name: "SiteMap",
+                    template: "sitemap.xml",
+                    defaults: new { controller = "home", action = "SiteMap" });
+
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
